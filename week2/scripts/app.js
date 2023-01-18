ae6f699719e3dd845757f880039e8e6b696715b9
prerakkumarpatel <prerakkumar.patel@dcmail.ca>
2023-01-18 17:54:30 -0500

    WebD 6201 Week2 Initial Commit

--- week2/scripts/app.js
+++ week2/scripts/app.js
@@ -1,8 +1,42 @@
 "use strict";
 (function () {
-    function Start() {
-        console.log("app started")
+    function DisplayHomePage() {
+        {
+            let AboutUsButton = document.getElementById("AboutUsBtn") ;
+            AboutUsButton.addEventListener("click",DisplayHomePage);
+            console.log("About Us Button clicked");
+            location.href="about.html";
+        };
     }
-    window.addEventListener("load",Start)
+    function DisplayProductPage(){}
+    function DisplayServicePage(){}
+    function DisplayAboutPage(){}
+    function DisplayContactPage(){}
 
-})();
+    function Start() {
+        console.log("app started");
+        switch(document.title)
+        {
+            case "Home":
+                DisplayHomePage();
+                break;
+            case "Product":
+                DisplayProductPage();
+                break;
+            case "About":
+                DisplayAboutPage();
+                break;
+            case "Service":
+                DisplayServicePage();
+                break;
+            case "Contact":
+                DisplayContactPage();
+                break;
+        }
+
+
+    }
+
+    window.addEventListener("load",Start);
+
+})()
