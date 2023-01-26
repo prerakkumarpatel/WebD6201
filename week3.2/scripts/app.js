e056934d053bfbf6673df805daf9909e375c664b
prerakkumarpatel <prerakkumar.patel@dcmail.ca>
2023-01-25 23:42:20 -0500

    Assignment 1 week3.2 completed without final review

--- week3.2/scripts/app.js
+++ week3.2/scripts/app.js
@@ -6,7 +6,26 @@
             console.log("About Us Button clicked");
             location.href = "about.html";
         });
-    }
+
+                let MainContent= document.getElementsByTagName("main")[0];
+                let MainParagraph = document.createElement("p");
+                MainParagraph.setAttribute("id","MainParagraph");
+                MainParagraph.setAttribute("class","mt-3");
+                MainParagraph.textContent="This is javascript generated paragraph";
+                MainContent.appendChild(MainParagraph);
+                //
+                let FirstString = "This is {concatenated}";
+                let SecondString = `${FirstString} the Main Paragraph.` ;
+                MainParagraph.textContent= SecondString;
+
+                let Article = document.createElement("article");
+                let ArticleParagraph = `<p id="ArticleParagraph" class="mt-3">This is Article paragraph</p>`;
+                Article.setAttribute("class","container");
+                Article.innerHTML=ArticleParagraph;
+                document.body.appendChild(Article);
+            }
+
+
     function DisplayProductPage(){
         console.log("Display Product page called");
     }
